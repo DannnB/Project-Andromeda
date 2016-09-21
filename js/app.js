4cee9e9947fd5ca56c06e05a719462273f4eefa7
DannnB <dbroughton_09@hotmail.com>
2016-09-21 21:37:50 +0100

    Set up booking app module, get json data from file with $getJSON = $.ajax shorthand, add button to test GET data from parsed file

--- js/app.js
+++ js/app.js
@@ -11,4 +11,27 @@ $(window).load(function () {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     
+    
+function bookingApp(){
+    // if this dosen't pop up they we broke it somewhere out of this function most likely 
+    console.log("Booking App func Started, don't let your dreams be dreams! Lets do stuff!");
+    
+    
+    
+    // Init datepicker
+        // beforeDay: add booked day for current pod/pitches
+    
+    // on input select
+        // get selectedDate store in var
+            //
+    
+    // Check JSON 
+    
+    // update number of days aval
+    
+    //
+}   
+    
+    // Make it standalone module
+    bookingApp();
 });
